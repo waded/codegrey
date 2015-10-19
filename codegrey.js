a5360fbc93e491ba291beace6fcf890436382e9a
Wade Dorrell <wade@dorrells.org>
2015-10-19 17:29:36 -0600

    Good enough

---
+++ codegrey.js
@@ -0,0 +1,9 @@
+var app = require("express")();
+
+app.use("/:code", function(req, res) {
+  res.set('X-Powered-By', 'codegrey');
+  res.status(req.params.code);
+  res.send(req.params.code + "'d!");
+});
+
+app.listen(process.env.PORT || 8888);
